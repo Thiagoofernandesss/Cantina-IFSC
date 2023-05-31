4101e7ad0d76d748a175d3a267229f0bcb3a86f8
Thiagofernandesss <thiago.f.l.costa@hotmail.com>
2023-05-31 13:40:39 -0300

    Atualizacao31.05

--- src/Dao/BairroDao.java
+++ src/Dao/BairroDao.java
@@ -28,6 +28,48 @@ public class BairroDao {
         }
         return instance;
     }
+    
+    public void adicionarBairro(Bairro bairro){
+        bairros.add(bairro);
+    }
+    
+    public void atualizaBairro(Bairro bairro){
+        for(Bairro b : bairros){
+            if(b.getId() == bairro.getId()){
+                b.setDescricao(bairro.getDescricao());
+                b.setId(bairro.getId());
+                break;
+            }
+        }
+        
+    }
+    
+    public void deletarBairro(int bairroId){
+        Bairro bairroToRemove =null;
+        
+        for(Bairro b : bairros){
+            if(b.getId() == bairroId){
+                bairroToRemove = b;
+                break;
+            }
+        }
+        if(bairroToRemove!= null){
+            bairros.remove(bairroToRemove);
+        }
+    }
+    
+    public List<Bairro> getAllBairros(){
+        return bairros;
+    }
+    
+    public Bairro getBairroById(int BairroId){
+        for(Bairro b: bairros){
+            if(b.getId() == BairroId){
+                return b;
+            }
+        }
+        return null;
+    }
 
 
     
