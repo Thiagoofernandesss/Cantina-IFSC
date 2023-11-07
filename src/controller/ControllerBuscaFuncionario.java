c2866596462bbe01323a37bcac33fa7d4951d1ab
Gabrieli Ribeiro <gabrielirt@hotmail.com>
2023-11-06 23:18:56 -0300

    Enviando alterações de Controller de Funcionário

--- src/controller/ControllerBuscaFuncionario.java
+++ src/controller/ControllerBuscaFuncionario.java
@@ -6,11 +6,15 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import model.bo.Funcionario;
 import view.BuscaFuncionario;
 
 /**
  *
- * @author Thiago
+ * @author Thiago and Gabrieli
  */
 public class ControllerBuscaFuncionario implements ActionListener{
     
@@ -18,21 +22,46 @@ public class ControllerBuscaFuncionario implements ActionListener{
 
     public ControllerBuscaFuncionario(BuscaFuncionario buscaFuncionario) {
         this.buscaFuncionario = buscaFuncionario;
-        
         this.buscaFuncionario.getjButtonFiltrar().addActionListener(this);
         this.buscaFuncionario.getjButtonCarregar().addActionListener(this);
+        this.buscaFuncionario.getjComboBoxBuscaFuncionariosPor().addActionListener(this);
         this.buscaFuncionario.getjButtonSair().addActionListener(this);
-        
-        utilities.Utilities.ativa(true, this.buscaFuncionario.getjPanelBotoes());
+        this.buscaFuncionario.getjTextFieldFiltrar().addActionListener(this);
     }
-    
     
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == this.buscaFuncionario.getjButtonFiltrar()){
-        
+            if (!this.buscaFuncionario.getjTextFieldFiltrar().getText().trim().equalsIgnoreCase("")) {
+                List<Funcionario> listaFuncionarios = new ArrayList<Funcionario>();
+                
+                if(this.buscaFuncionario.getjComboBoxBuscaFuncionariosPor().getSelectedItem().toString().equalsIgnoreCase("id")) {
+                    listaFuncionarios.add(service.FuncionarioService.carregar(Integer.parseInt(this.buscaFuncionario.getjTextFieldFiltrar().getText())));
+                }else {
+                    listaFuncionarios = service.FuncionarioService.carregar(this.buscaFuncionario.getjComboBoxBuscaFuncionariosPor().getSelectedItem().toString().toLowerCase(),
+                            this.buscaFuncionario.getjTextFieldFiltrar().getText());
+                }
+                
+                //Criando um objeto do tipo table model
+                DefaultTableModel tabela = (DefaultTableModel) this.buscaFuncionario.getjTableDados().getModel();
+                tabela.setRowCount(0);
+
+                for (Funcionario funcionarioAtual : listaFuncionarios) {
+                    tabela.addRow(new Object[]{funcionarioAtual.getId(),
+                        funcionarioAtual.getNome(),
+                        funcionarioAtual.getCpf(),
+                        funcionarioAtual.getRg(),
+                        funcionarioAtual.getUsuario(),
+                        funcionarioAtual.getStatus()});
+
+                }
+            }
         }else if(e.getSource() == this.buscaFuncionario.getjButtonCarregar()){
+            controller.ControllerCadastroFuncionario.codigo = (int) this.buscaFuncionario.getjTableDados().
+                    getValueAt(this.buscaFuncionario.getjTableDados().getSelectedRow(), 0);
+
+            this.buscaFuncionario.dispose();
         
         }else if(e.getSource() == this.buscaFuncionario.getjButtonSair()){
             this.buscaFuncionario.dispose();
